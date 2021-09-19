a8867591fc338ec1ed5dc8d70644b113294a18f2
ana <ana.maticic@protonmail.com>
2021-09-19 19:13:30 +0200

    solve desending order kata

--- src/DescendingOrder.java
+++ src/DescendingOrder.java
@@ -1,9 +1,23 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class DescendingOrder {
     public static int sortDesc(final int num) {
+
         String word = String.valueOf(num);
+        StringBuilder sbf = new StringBuilder();
+        Character[] arr = new Character[word.length()];
+
         for (int i = 0; i < word.length(); i++) {
-            String[] arr = [i];
+            arr[i] = word.charAt(i);
         }
-        return 0;
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (Character character : arr) {
+            sbf.append(character);
+        }
+
+        return Integer.parseInt(String.valueOf(sbf));
     }
 }
