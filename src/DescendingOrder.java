aab64a245ea591e04a782610b91f154ef5c80269
ana <ana.maticic@protonmail.com>
2021-09-19 18:41:14 +0200

    Add descending order kata

---
+++ src/DescendingOrder.java
@@ -0,0 +1,9 @@
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+        String word = String.valueOf(num);
+        for (int i = 0; i < word.length(); i++) {
+            String[] arr = [i];
+        }
+        return 0;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DescendingOrder.sortDesc(123456789));
+    }
+}
