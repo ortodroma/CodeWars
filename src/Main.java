82306a5a822b763ce14a299045479592e848ca8a
ana <ana.maticic@protonmail.com>
2021-09-24 13:50:45 +0200

    solve kata MakeUpperCase

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         char l;
-        System.out.println(AllStarCodeChallenge_18.strCount("Hello",'l'));
+        System.err.println(MakeUpperCase.MakeUpperCase("Hello"));
     }
 }
