7929ca90463687b9233b432d89520ef5742eb514
ortodroma <86344670+ortodroma@users.noreply.github.com>
2021-10-05 20:57:08 +0200

    Revert "Add What is between solution (#3)"
    This reverts commit ca165920638aac5bef0c91e99a4aee4bcede5806.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        int[] arr = WhatIsBetween.between(7, 12);
-        System.err.println(Arrays.toString(WhatIsBetween.between(7, 12)));
-
+        char l;
+        System.err.println(MakeUpperCase.MakeUpperCase("Hello"));
     }
 }
