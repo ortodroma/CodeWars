422327ae9298e0282b7f1dd55d9a575984218fb8
ana <ana.maticic@protonmail.com>
2021-10-05 15:43:14 +0200

    Solve kata What is Between

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] arr = WhatIsBetween.between(7, 12);
-        System.err.println(Arrays.toString(WhatIsBetween.between(7,12)));
+        System.err.println(Arrays.toString(WhatIsBetween.between(7, 12)));
 
     }
 }
--- src/WhatIsBetween.java
+++ src/WhatIsBetween.java
@@ -1,16 +1,12 @@
 public class WhatIsBetween {
     public static int[] between(int a, int b) {
 
-        int[] arr = new int[b - a+1];
-        for (int i = 0; i < b - a+1; i++) {
+        int[] arr = new int[b - a + 1];
+        for (int i = 0; i < b - a + 1; i++) {
             arr[i] = a + i;
         }
         return arr;
     }
-
-   /* @Override
-    public String toString() {
-        return ;
-    }*/
+    
 }
 
