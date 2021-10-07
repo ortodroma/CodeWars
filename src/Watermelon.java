753d39ed659331bfa5003beecf04f759863a9636
ortodroma <86344670+ortodroma@users.noreply.github.com>
2021-10-07 15:27:43 +0200

    Solve kata Watermelon (#6)

---
+++ src/Watermelon.java
@@ -0,0 +1,9 @@
+public class Watermelon {
+    public static boolean divide(int weight) {
+
+        if (weight>2 && weight%2==0) {
+            return true;
+        }
+        return false;
+    }
+}
