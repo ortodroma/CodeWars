62c5f042d78c65ea53580a890bde9a5f25be60ae
ana <ana.maticic@protonmail.com>
2021-10-19 19:12:36 +0200

    prva verzija

---
+++ src/rectangle/CustomRectangle.java
@@ -0,0 +1,33 @@
+package rectangle;
+
+import java.awt.*;
+
+public class CustomRectangle {
+    private  int x;
+    private  int y;
+    private  int width;
+    private  int height;
+
+    public CustomRectangle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
---
+++ src/rectangle/README.md
@@ -0,0 +1,1 @@
+Write a program that figures out if 2 rectangles intersect with each other. A program must accept coordinates of 2 rectangles in a 2-D plane. It should return true if the rectangles intersect and false otherwise. The data structures used and programming language chosen (can also be pseudocode) is up to the candidate.
